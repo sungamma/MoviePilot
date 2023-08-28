30ad29347d72be111ce411e482ab3a1b9a5ad538
sungamma <sungamma@gmail.com>
2023-08-28 11:33:48 +0800

    feat:自动更新编译文件并编译

---
+++ .github/workflows/merge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+diff build-docker.yml build-docker.yml.bak > /dev/null
+if [  "$?" == "0" ]; then #"$?"是上一执行命令的返回值。
+    echo "nothing to change"
+else
+    cp build-docker.yml build-docker.yml.bak
+    cp build-docker.yml build-docker-auto.yml
+    sudo snap install yq
+    yq e '.on = "push"' -i build-docker-auto.yml
+    yq e '.name = "MoviePilot Docker Auto"' -i build-docker-auto.yml && echo "已更新编译文件"
+    git add .
+    git commit -m "update build-docker-auto.yml"
+fi
